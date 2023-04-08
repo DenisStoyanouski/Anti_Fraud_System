6c356e1feb6b076d0187b69c7363a75dc6fdd712
Denis Stoyanouski <vandenst@tut.by>
2023-04-08 14:42:31 +0300

    add class Transaction and TransactionController

---
+++ src/antifraud/businesslayer/Transaction.java
@@ -0,0 +1,16 @@
+package antifraud.businesslayer;
+
+public class Transaction {
+    Long amount;
+
+    public Transaction() {
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/antifraud/presentation/TransactionController.java
@@ -0,0 +1,34 @@
+package antifraud.presentation;
+
+import antifraud.businesslayer.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+
+@RestController
+public class TransactionController {
+
+    @PostMapping(path = "/api/antifraud/transaction", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseEntity makePurchase(@RequestBody Transaction transaction) {
+        Map<String, String> response;
+        long amount = transaction.getAmount();
+        if (amount > 0 && amount <= 200) {
+            response = Map.of("result", "ALLOWED");
+            return ResponseEntity.ok().body(response);
+        } else if (amount > 200 && amount <= 1500) {
+            response = Map.of("result", "MANUAL_PROCESSING");
+            return ResponseEntity.ok().body(response);
+        } else if (amount > 1500) {
+            response = Map.of("result", "PROHIBITED");
+            return ResponseEntity.ok().body(response);
+        }
+        return ResponseEntity.badRequest().body(HttpStatus.BAD_REQUEST);
+    }
+}
