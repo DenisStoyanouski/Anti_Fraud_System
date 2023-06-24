caa70721394794a29e1c71efad9e66a3ca682cce
Denis Stoyanouski <vandenst@tut.by>
2023-06-24 19:42:01 +0300

    Added UserDetailsImpl and UserDetailsServiceImpl classes

--- src/antifraud/businesslayer/User.java
+++ src/antifraud/businesslayer/User.java
@@ -3,15 +3,21 @@ package antifraud.businesslayer;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
+    long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String username;
+    @NotBlank
     @JsonProperty(required = true, access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-    //private String role; // should be prefixed with ROLE_
+    private String role; // should be prefixed with ROLE_
 }
