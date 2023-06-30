0ccf93d430a23be64e3ed81e6380b61fe086cea8
Denis Stoyanouski <vandenst@tut.by>
2023-06-30 22:10:56 +0300

    added IpAddress class

---
+++ src/antifraud/IpAddress/IpAddress.java
@@ -0,0 +1,25 @@
+package antifraud.IpAddress;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@Entity
+@Table(name="ip_address")
+public class IpAddress {
+    @Id
+    @GeneratedValue
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    long id;
+    @NotBlank
+    @Pattern(regexp = "^((\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(?!$)|$)){4}$")
+    private String ip;
+}
