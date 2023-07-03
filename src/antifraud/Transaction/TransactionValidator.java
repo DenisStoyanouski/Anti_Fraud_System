ed14c4bc16a4665d27f43e1f0cc993d3b6e6d305
Denis Stoyanouski <vandenst@tut.by>
2023-07-03 15:47:39 +0300

    added methods to TransactionRepository

--- src/antifraud/Transaction/TransactionValidator.java
+++ src/antifraud/Transaction/TransactionValidator.java
@@ -5,6 +5,7 @@ import antifraud.IpAddress.IpAddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,11 +17,12 @@ public class TransactionValidator {
     private String result;
     private List<String> info;
 
+    private Transaction transaction;
+
     @Autowired
     public TransactionValidator(IpAddressService ipAddressService,
                                 CardService cardService,
-                                TransactionRepository transactionRepository)
-    {
+                                TransactionRepository transactionRepository) {
         this.ipAddressService = ipAddressService;
         this.cardService = cardService;
         this.transactionRepository = transactionRepository;
@@ -28,17 +30,20 @@ public class TransactionValidator {
     }
 
     public Map<String, String> getResult(Transaction transaction) {
+        this.transaction = transaction;
         info = new ArrayList<>();
-        validateAmount(transaction);
-        validateCardNumber(transaction);
-        validateIpAddress(transaction);
+        validateAmount();
+        validateCardNumber();
+        validateIpAddress();
+        validateRegionLastHour();
+        validateIpAddressLastHour();
         Map<String, String> validationResult = new LinkedHashMap<>();
         validationResult.put("result", result);
         validationResult.put("info", info.size() == 0 ? "none" : info.stream().sorted().collect(Collectors.joining(", ")));
         return validationResult;
     }
 
-    private void validateAmount(Transaction transaction) {
+    private void validateAmount() {
         long amount = transaction.getAmount();
         if (amount > 0 && amount <= 200) {
             result = Result.ALLOWED.name();
@@ -51,23 +56,53 @@ public class TransactionValidator {
         }
     }
 
-    private void validateCardNumber(Transaction transaction) {
+    private void validateCardNumber() {
         if (cardService.existByNumber(transaction.getNumber())) {
-            if (Objects.equals(result, Result.MANUAL_PROCESSING.name())) {
-                info.remove("amount");
-            }
             result = Result.PROHIBITED.name();
             info.add("card-number");
         }
     }
 
-    private void validateIpAddress(Transaction transaction) {
+    private void validateIpAddress() {
         if (ipAddressService.existByIp(transaction.getIp())) {
-            if (Objects.equals(result, Result.MANUAL_PROCESSING.name())) {
-                info.remove("amount");
-            }
             result = Result.PROHIBITED.name();
             info.add("ip");
         }
+    }
+
+    private void validateRegionLastHour() {
+        long numberOfRegions = transactionRepository.countTransactionsFromTwoAnotherRegionsInLastHour(
+                transaction.getRegion(),
+                transaction.getLocalDateTime().minus(1, ChronoUnit.HOURS),
+                transaction.getLocalDateTime());
+
+        if (numberOfRegions == 2 && !Objects.equals(result, Result.PROHIBITED.name())) {
+            result = Result.MANUAL_PROCESSING.name();
+            info.add("region-correlation");
+        } else if (numberOfRegions > 2) {
+            if (!Objects.equals(result, Result.PROHIBITED.name())) {
+                result = Result.PROHIBITED.name();
+                info.clear();
+            }
+            info.add("region-correlation");
+        }
+    }
+
+    private void validateIpAddressLastHour() {
+        long numberOfAddresses = transactionRepository.countTransactionsFromTwoAnotherIpAddressInLastHour(
+                transaction.getIp(),
+                transaction.getLocalDateTime().minus(1, ChronoUnit.HOURS),
+                transaction.getLocalDateTime());
+        if (numberOfAddresses == 2 && !Objects.equals(result, Result.PROHIBITED.name())) {
+            result = Result.MANUAL_PROCESSING.name();
+            info.add("ip-correlation");
+        } else if (numberOfAddresses > 2) {
+            if (!Objects.equals(result, Result.PROHIBITED.name())) {
+                result = Result.PROHIBITED.name();
+                info.clear();
+            }
+            info.add("ip-correlation");
+        }
+
     }
 }
